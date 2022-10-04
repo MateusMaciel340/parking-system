966b13f21541876a46ae0c839fb0c9361b712d57
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-04 16:01:14 -0300

    feat: definition of initial/hourly variables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using parking_system.Models;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal initialPrice = 0;
+decimal pricePerHour = 0;
+
+Console.WriteLine("Seja bem vindo ao estacionamento!\n" +
+    "Digite o preço inicial: ");
+initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora: ");
+pricePerHour = Convert.ToDecimal(Console.ReadLine());
